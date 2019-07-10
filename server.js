b191e9bcf35dc940824a8980a28b4de5f557c417
openwell <timileyinojo@yahoo.com>
2019-07-10 15:12:23 +0100

    added logger middleware

--- server.js
+++ server.js
@@ -2,16 +2,21 @@ const express = require("express");
 const userRoute = require("./posts/postRouter");
 const server = express();
 
+server.use(logger)
+server.use("/user", userRoute);
+
 server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
-server.use("/user", userRoute);
 
 server.all("*", (req, res) => {
   res.redirect(301, "/user");
 });
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(req.method, req.url, Date.now());
+  next();
+}
 
 module.exports = server;
