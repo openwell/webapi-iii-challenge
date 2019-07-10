f145cf4fefc57ff3421a9cba06da9397cfb0e64a
openwell <timileyinojo@yahoo.com>
2019-07-10 14:42:39 +0100

    setting up of the app and installing dependencies

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-const express = 'express';
-
+const express = require("express");
+const userRoute = require("./posts/postRouter");
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+server.get("/", (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
 });
+server.use("/user", userRoute);
 
+server.all("*", (req, res) => {
+  res.redirect(301, "/user");
+});
 //custom middleware
 
-function logger(req, res, next) {
-
-};
+function logger(req, res, next) {}
 
 module.exports = server;
